1e7b9db8b511c59aaf62a456211767276def2b0a
Tony Brock <abrock17@gmail.com>
2016-11-26 12:45:57 -0500

    recursive calls to build operation (and clean up) - at last!

--- rpn-converter.c
+++ rpn-converter.c
@@ -14,8 +14,8 @@ struct Operation {
     char operator;
     char left_operand;
     char right_operand;
-    Operation *left_operation;
-    Operation *right_operation;
+    Operation *left_sub_operation;
+    Operation *right_sub_operation;
 };
 
 const char supported_operators[] = { '^', '/', '*', '-', '+' };
@@ -53,22 +53,87 @@ static bool is_valid_operand(const char operand) {
     return operand >= min_char_int_value && operand <= max_char_int_value;
 }
 
-rpn_conversion_status to_rpn(const char *infix, char *rpn) {
-    Operation operation = { null_char, null_char, null_char, NULL, NULL };
-    operation.operator = infix[1];
-    operation.left_operand = infix[0];
-    operation.right_operand = infix[2];
-    if (!is_supported_operator(operation.operator) || 
-        !is_valid_operand(operation.left_operand) || 
-        !is_valid_operand(operation.right_operand)) {
+static void clean_up_operation(Operation *operation) {
+    if (operation) {
+        clean_up_operation(operation->left_sub_operation);
+        clean_up_operation(operation->right_sub_operation);
+        free(operation);
+    }
+}
+
+static Operation *initialize_operation(char operator) {
+    Operation *operation = (Operation *) malloc(sizeof(Operation));
+
+    operation->operator = operator;
+    operation->left_operand = null_char;
+    operation->right_operand = null_char;
+    operation->left_sub_operation = NULL;
+    operation->right_sub_operation = NULL;
+    return operation;
+}
+
+static rpn_conversion_status build_operation_from_infix(const char *infix, Operation **operation) {
+    // printf("populate operation from : %s\n", infix);
+    *operation = initialize_operation(infix[1]);
+
+    (*operation)->left_operand = infix[0];
+
+    const char *right_side = &infix[2];
+    // printf("right side : %s\n", right_side);
+    if (strlen(right_side) > 1) {
+        // Operation right_sub_operation = { null_char, null_char, null_char, NULL, NULL };
+        // operation->right_sub_operation = &right_sub_operation;
+        build_operation_from_infix(right_side, &(*operation)->right_sub_operation);
+        // printf("right side operator : %c, left_operand : %c, right_operand : %c\n", 
+        //     right_sub_operation.operator, right_sub_operation.left_operand, right_sub_operation.right_operand);
+    } else {
+        (*operation)->right_operand = right_side[0];
+    }
+    
+    if (!is_supported_operator((*operation)->operator) || 
+        !is_valid_operand((*operation)->left_operand) || 
+        (null_char != (*operation)->right_operand && !is_valid_operand((*operation)->right_operand))) {
         return INVALID_CHARACTER;
     }
 
-    rpn[2] = operation.operator;
-    rpn[0] = operation.left_operand;
-    rpn[1] = operation.right_operand;
-
     return SUCCESS;
+}
+
+static void populate_rpn_from_operation(const Operation *operation, char *rpn, int *start_index, int *end_index) {
+    rpn[*start_index] = operation->left_operand;
+    (*start_index)++;
+    // printf("operation.operator : %c, operation.left_operand : %c, operation.right_operand : %c, : rpn %s\n", 
+        // operation->operator, operation->left_operand, operation->right_operand, rpn);
+    if (NULL != operation->right_sub_operation) {
+        // printf("right_sub_operation operator : %c, left_operand : %c\n", 
+            // operation->right_sub_operation->operator, operation->right_sub_operation->left_operand);
+        populate_rpn_from_operation(operation->right_sub_operation, rpn, start_index, end_index);
+    } else {
+        rpn[*start_index] = operation->right_operand;
+        (*start_index)++;
+    }
+
+    rpn[*end_index] = operation->operator;
+    (*end_index)--;
+}
+
+rpn_conversion_status to_rpn(const char *infix, char *rpn) {
+    Operation *operation = NULL;
+    rpn_conversion_status status = build_operation_from_infix(infix, &operation);
+    // printf("root operation operator : %c, left_operand : %c, right_operand : %c\n", 
+    //     operation.operator, operation.left_operand, operation.right_operand);
+    if (SUCCESS != status) {
+        clean_up_operation(operation);
+        return status;
+    }
+
+    int start_index = 0, end_index = strlen(infix) - 1;
+
+    populate_rpn_from_operation(operation, rpn, &start_index, &end_index);
+
+    clean_up_operation(operation);
+
+    return status;
 }
 
 rpn_conversion_status to_rpn_lame(const char *infix, char *rpn) {
